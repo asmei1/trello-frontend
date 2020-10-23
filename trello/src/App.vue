dc873a8b00a7ca16bb580b433a9e9cf4038a6ff3
Asmei <kamsykula@gmail.com>
2020-10-23 22:03:52 +0200

    Added Hello world

---
+++ trello/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<div id="app">
+    <p>{{ greeting }}</p>
+    <p>{{ flaskGreeting }}</p>
+</div>
+</template>
+
+<script>
+export default {
+    components: {
+    },
+    data: function(){
+        return {
+            greeting: 'Hello, Vue!',
+            flaskGreeting: ''
+        }
+    },
+    created: async function(){
+        const gResponse = await fetch("http://localhost:5000/");
+        const gObject = await gResponse.json();
+        this.flaskGreeting = gObject.greeting;
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
