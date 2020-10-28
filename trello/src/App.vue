fd0af388c550416aa39cf32c8ae70b46b2a59898
Paulina Kośka <paulina.koska@onet.pl>
2020-10-28 17:17:33 +0100

    Changed UI

--- trello/src/App.vue
+++ trello/src/App.vue
@@ -1,39 +1,69 @@
 <template>
-<div id="app">
-    <p>{{ greeting }}</p>
-    <p>{{ flaskGreeting }}</p>
-    <p>{{ teamName }}</p>
-</div>
+  <div id=app>
+    <main>
+      <div class="container">
+      <h1 class="display-3">{{ greeting }}</h1>
+      <h2 class="display-3">{{ flaskGreeting }}</h2>
+      <h3 class="display-3">{{ teamName }}</h3>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
 
 export default {
-    components: {
-    },
-    data: function(){
-        return {
-            greeting: 'Hello, Vue!',
-            flaskGreeting: '',
-            teamName: ''
-        }
-    },
-    created: async function(){
-        const gResponse = await fetch("http://localhost:5000/");
-        const gObject = await gResponse.json();
-        this.flaskGreeting = gObject.greeting;
-        this.teamName = gObject.team;
+  components: {},
+  data: function () {
+    return {
+      greeting: 'Hello, Vue!',
+      flaskGreeting: '',
+      teamName: ''
     }
+  },
+  created: async function () {
+    const gResponse = await fetch("http://localhost:5000/");
+    const gObject = await gResponse.json();
+    this.flaskGreeting = gObject.greeting;
+    this.teamName = gObject.team;
+  }
 }
 </script>
 
 <style>
-#app {
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+#app {
+  background-image: url('./assets/vue.jpeg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main {
+  min-height: 100vh;
+  padding-top: 200px;
+  padding-left: 70vh;
+}
+
+.container {
+  background-color: white;
+  border-color: #42b983;
+  border-style: dashed;
+  border-radius: 14px;
+  width: 600px;
+  height: 200px;
+}
+.display-3 {
+  padding: 20px;
 }
 </style>
