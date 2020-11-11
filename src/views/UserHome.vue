a302826b5d16aac4928461306e4febf7ef7adba1
Asmei <kamsykula@gmail.com>
2020-11-11 01:19:55 +0100

    Using username and token from store

--- src/views/UserHome.vue
+++ src/views/UserHome.vue
@@ -2,36 +2,42 @@
   <section>
     <div class="full-control">
 
-        <md-toolbar style="color: white; background-color: #0079BF;">
-          <h3 class="md-title" style="color: white;">Active tables</h3>
-        </md-toolbar>
-        <div v-for="(board) in nonArchivedBoards" v-bind:key="board.id">
-          <md-button class="md-raised">{{ board }}</md-button>
-        </div>
-        <md-button class="md-raised" style="color: black; background-color: #F0F0F0;">Create new table</md-button>
-        <md-toolbar style="color: white; background-color: dimgray;">
-          <h3 class="md-title" style="color: white;">Archived tables</h3>
-        </md-toolbar>
-        <div v-for="(board) in archivedBoards" v-bind:key="board.id">
-          <md-button class="md-raised">{{ board}}</md-button>
-        </div>
+      <md-toolbar style="color: white; background-color: #0079BF;">
+        <h3 class="md-title" style="color: white;">Active tables</h3>
+      </md-toolbar>
+      <div v-for="(board) in nonArchivedBoards" v-bind:key="board.id">
+        <md-button class="md-raised">{{ board }}</md-button>
+      </div>
+      <md-button class="md-raised" style="color: black; background-color: #F0F0F0;">Create new table</md-button>
+      <md-toolbar style="color: white; background-color: dimgray;">
+        <h3 class="md-title" style="color: white;">Archived tables</h3>
+      </md-toolbar>
+      <div v-for="(board) in archivedBoards" v-bind:key="board.id">
+        <md-button class="md-raised">{{ board }}</md-button>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-name: "UserHome",
-  data(){
-  return{
-    archivedBoards: [],
-    nonArchivedBoards: []
+  name: "UserHome",
+  data() {
+    return {
+      archivedBoards: [],
+      nonArchivedBoards: []
     }
   },
   created: async function () {
 
+    const headers_with_tokens = new Headers();
+    headers_with_tokens.append("Authorization", "Bearer " + this.$store.state.token);
 
-    let response = await fetch(`http://localhost:5000/get_user_boards?username=User`);
+    let response = await fetch(`http://localhost:5000/get_user_boards?username=` + this.$store.state.user.username,
+        {
+          method: "GET",
+          headers: headers_with_tokens
+        });
     let data = await response.json()
     this.archivedBoards = data.archieve_boards;
     this.nonArchivedBoards = data.non_archieve_boards;
@@ -43,14 +49,14 @@ name: "UserHome",
 
 <style lang="scss" scoped>
 
-section{
+section {
   height: 100vh;
   background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('../assets/homeUserBackground.jpg');
   background-position: top left;
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-size: cover;
-  font-family: "Segoe Print",serif;
+  font-family: "Segoe Print", serif;
 
 }
 
@@ -82,18 +88,19 @@ section{
   justify-content: center;
 }
 
-.md-raised{
+.md-raised {
   height: 100px;
   width: 200px;
   border-radius: 10px;
 }
 
-.md-toolbar{
+.md-toolbar {
   margin-top: 20px;
   margin-bottom: 20px;
   background-color: #eca3cb;
 }
-.md-content{
+
+.md-content {
   height: 400px;
 }
 </style>
